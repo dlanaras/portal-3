6127f9c9a8107ca46f292e5ebc0a5354c94316fe
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-03-30 16:27:35 +0200

    feat!: a lot of progress

---
+++ include/arduino_secrets_example.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID = "MUCH"
+#define SECRET_PASS = "WOW"
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,44 @@
 #include <Arduino.h>
 #include <SPI.h>
+
 #include <Wire.h>
 #include <Adafruit_AMG88xx.h>
 
+#include "uRTCLib.h"
+
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+#define PIN_RELAY_PUMP 1
+
+int status = WL_IDLE_STATUS;
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID; // your network SSID (name)
+char pass[] = SECRET_PASS; // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;          // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 2390; // local port to listen on
+
+char packetBuffer[256]; // buffer to hold incoming packet
+
 Adafruit_AMG88xx amg;
+
+uRTCLib rtc(0x68);
+
+float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 
 void setup()
 {
+  pinMode(PIN_RELAY_PUMP, OUTPUT);
+
   Serial.begin(9600);
-  Serial.println(F("AMG88xx test"));
+  Serial.println(F("AMG88xx pixels"));
+
+  URTCLIB_WIRE.begin();
+
+
+  //rtc.set(0, 04, 15, 5, 30, 3, 23);
 
   bool status;
 
@@ -21,7 +51,7 @@ void setup()
       ;
   }
 
-  Serial.println("-- Thermistor Test --");
+  Serial.println("-- Pixels Test --");
 
   Serial.println();
 
@@ -30,12 +60,47 @@ void setup()
 
 void loop()
 {
-  Serial.print("Thermistor Temperature = ");
+  // read all the pixels
+  amg.readPixels(pixels);
+
+  for (int i = 1; i <= AMG88xx_PIXEL_ARRAY_SIZE; i++)
+  {
+    Serial.print(pixels[i - 1]);
+    Serial.print(", ");
+    if (i % 8 == 0)
+      Serial.println();
+  }
+  Serial.println();
+
+  delay(1000);
+
+  rtc.refresh();
+
+  Serial.print("RTC DateTime: 20");
+  Serial.print(rtc.year());
+  Serial.print('-');
+  Serial.print(rtc.month());
+  Serial.print('-');
+  Serial.print(rtc.day());
+
+  Serial.print(' ');
+
+  Serial.print(rtc.hour());
+  Serial.print(':');
+  Serial.print(rtc.minute());
+  Serial.print(':');
+  Serial.print(rtc.second());
+
+  Serial.print(" DOW: ");
+  Serial.print(rtc.dayOfWeek());
+
+  Serial.print(" - Temp: ");
   Serial.print(amg.readThermistor());
-  Serial.println(" *C");
 
   Serial.println();
 
+  digitalWrite(PIN_RELAY_PUMP, HIGH);
   // delay a second
-  delay(1000);
+  delay(2000);
+  digitalWrite(PIN_RELAY_PUMP, LOW);
 }
