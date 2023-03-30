1dc5211805721c866f51936db58ee8f1c46ba761
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-03-30 13:56:42 +0200

    feat: thermal camera test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_AMG88xx.h>
 
-void setup() {
-  // put your setup code here, to run once:
+Adafruit_AMG88xx amg;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("AMG88xx test"));
+
+  bool status;
+
+  // default settings
+  status = amg.begin();
+  if (!status)
+  {
+    Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
+    while (1)
+      ;
+  }
+
+  Serial.println("-- Thermistor Test --");
+
+  Serial.println();
+
+  delay(100); // let sensor boot up
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.print("Thermistor Temperature = ");
+  Serial.print(amg.readThermistor());
+  Serial.println(" *C");
+
+  Serial.println();
+
+  // delay a second
+  delay(1000);
 }
